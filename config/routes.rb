f11351be7e4dca5d3e2b3c2a58d955c65cefed97
vagrant <vagrant@kali>
2022-08-11 07:54:02 -0400

    03-routes-namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :books
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namepace :api do
+    namepace :v1 do
+      resources :books
+    end
+  end
 end
